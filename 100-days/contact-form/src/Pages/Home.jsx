76fb913c3e3384bcdf218854ea8acddfbfca0a1c
Rodrigo de Castro <rodrigo.cs.dev@gmail.com>
2022-05-26 16:54:05 -0300

    arquivos iniciais do projeto

---
+++ 100-days/contact-form/src/Pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Contact from '../Styles/home';
+
+function Home() {
+  return (
+    <Contact>
+      <header>
+        <h1>Contact Form</h1>
+        <h4>Please fill the text in the fields</h4>
+      </header>
+      <form>
+        <label htmlFor='name'>
+          Your Name: 
+          <input id='name' placeholder='Your Full Name'/>
+        </label>
+        <label htmlFor='email'>
+          Your Email: 
+          <input id='email' type='email' placeholder='Valid Email Address'/>
+        </label>
+        <label htmlFor='message'>
+          Message: 
+          <textarea id='email' type='message' />
+        </label>
+        <label htmlFor='subject'>
+          Subject: 
+          <input id='subject' placeholder='Job Inquiry'/>
+        </label>
+        <button>SEND</button>
+      </form>
+    </Contact>    
+  )
+}
+
+export default Home
